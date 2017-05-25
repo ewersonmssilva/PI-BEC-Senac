9dd3b0e5e9432ce31f61b3b7ff4a0ba9a5cd95a6
Ewerson <ewerson@msn.com>
2017-05-25 15:02:02 -0300

    Update and rename principal.c to principal2.c
    add segundo sensor

---
+++ principal2.c
@@ -0,0 +1,120 @@
+// Qual pino usar para ler o sensor? Pode usar qualquer pino!
+#define FLOWSENSORPIN 2
+
+#define FLOWSENSORPIN3 3
+
+// Conta quantos pulsos!
+volatile uint16_t pulses = 0;
+volatile uint16_t pulses3 = 0;
+// Rastrear o estado do pino de pulso
+volatile uint8_t lastflowpinstate;
+volatile uint8_t lastflowpinstate3;
+// Você pode tentar manter o tempo de quanto tempo é entre pulsos
+volatile uint32_t lastflowratetimer = 0;
+volatile uint32_t lastflowratetimer3 = 0;
+// E usar isso para calcular uma taxa de fluxo
+volatile float flowrate;
+volatile float flowrate3;
+// Interrupção é chamada uma vez por milissegundo, procura por pulsos do sensor!
+SIGNAL (TIMER0_COMPA_vect)
+{
+    uint8_t x = digitalRead (FLOWSENSORPIN);
+    uint8_t y = digitalRead (FLOWSENSORPIN3);
+
+    if (x == lastflowpinstate)
+    {
+        lastflowratetimer++;        
+        return; // nada mudou!
+    }
+
+    if (y == lastflowpinstate3)
+    {
+        lastflowratetimer3++;        
+        return; // nada mudou!
+    }   
+
+    if (x == HIGH)
+    {
+        //low to high  transição!
+        pulses++;        
+    }
+
+    if (y == HIGH)
+    {
+        //low to high  transição!
+        pulses3++;        
+    }
+
+    lastflowpinstate = x;
+    lastflowpinstate3 = y;
+    flowrate = 1000.0;
+    flowrate3 = 1000.0;
+    flowrate /= lastflowratetimer;  // Em hertz
+    flowrate3 /= lastflowratetimer3;  // Em hertz
+    lastflowratetimer = 0;
+    lastflowratetimer3 = 0;
+}
+
+void useInterrupt (boolean v)
+{
+    if (v)
+    {
+        // Timer0 já é usado para millis () - nós vamos apenas interromper em algum lugar
+        // no meio e chamar a função "Compare A" acima
+        OCR0A = 0xAF;
+        TIMSK0 |= _BV (OCIE0A);
+    }
+    else
+    {
+        // Não chamar mais a função de interrupção COMPA
+        TIMSK0 &= ~_BV (OCIE0A);
+    }
+}
+
+void setup()
+{
+    Serial.begin (9600);
+    Serial.print ("Sensor de fluxo OK!");
+
+    pinMode (FLOWSENSORPIN, INPUT);
+    pinMode (FLOWSENSORPIN3, INPUT);
+    digitalWrite (FLOWSENSORPIN, HIGH);
+    digitalWrite (FLOWSENSORPIN3, HIGH);
+    lastflowpinstate = digitalRead (FLOWSENSORPIN);
+    lastflowpinstate3 = digitalRead (FLOWSENSORPIN3);
+    useInterrupt (true);
+}
+
+void loop()                     // executar uma e outra vez
+{
+
+   // Serial.print ("Freq: ");
+   // Serial.println (flowrate);
+   // Serial.print ("Pulses: ");
+   // Serial.println (pulses, DEC);
+
+    // Se for um sensor plástico utilizar o seguinte cálculo
+    // Frequência do sensor (Hz) = 7.5 * Q (Liters/min)
+    // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
+    // Liters = (Frequency (Pulses/second) / 7.5) * time elapsed (seconds) / 60
+    // Liters = Pulses / (7.5 * 60)
+    float liters = pulses;
+    liters /= 7.5;
+    liters /= 60.0;
+
+    float liters3 = pulses3;
+    liters3 /= 7.5;
+    liters3 /= 60.0;
+
+    /*
+        // Se for um sensor de bronze utilizar o seguinte cálculo
+        float liters = pulses;
+        liters /= 8.1;
+        liters -= 6;
+        liters /= 60.0;
+    */
+    Serial.print (liters, liters3);
+    Serial.println ("S1 Litros","S2 Litros");
+
+    delay (100);
+}
