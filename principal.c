6ff9f3114c006c073b9502c455d44cf04c1cf811
Ewerson <ewerson@msn.com>
2017-05-31 11:53:08 -0300

    Update principal.c

--- principal.c
+++ principal.c
@@ -1,74 +1,77 @@
 // Qual pino usar para ler o sensor? Pode usar qualquer pino!
-#define FLOWSENSORPIN 2
+#define FLOWSENSORPIN1 2
 
-#define FLOWSENSORPIN3 3
+#define FLOWSENSORPIN2 3
 
 // Conta quantos pulsos!
 volatile uint16_t pulses = 0;
-volatile uint16_t pulses3 = 0;
+volatile uint16_t pulses2 = 0;
 // Rastrear o estado do pino de pulso
 volatile uint8_t lastflowpinstate;
-volatile uint8_t lastflowpinstate3;
+volatile uint8_t lastflowpinstate2;
 // Você pode tentar manter o tempo de quanto tempo é entre pulsos
 volatile uint32_t lastflowratetimer = 0;
-volatile uint32_t lastflowratetimer3 = 0;
+volatile uint32_t lastflowratetimer2 = 0;
 // E usar isso para calcular uma taxa de fluxo
 volatile float flowrate;
-volatile float flowrate3;
+volatile float flowrate2;
 // Interrupção é chamada uma vez por milissegundo, procura por pulsos do sensor!
-SIGNAL (TIMER0_COMPA_vect)
-{
-    uint8_t x = digitalRead (FLOWSENSORPIN);
-    uint8_t y = digitalRead (FLOWSENSORPIN3);
-
-    if (x == lastflowpinstate)
-    {
-        lastflowratetimer++;        
-        return; // nada mudou!
-    }
-
-    if (y == lastflowpinstate3)
-    {
-        lastflowratetimer3++;        
-        return; // nada mudou!
-    }   
-
-    if (x == HIGH)
-    {
-        //low to high  transição!
-        pulses++;        
-    }
-
-    if (y == HIGH)
-    {
-        //low to high  transição!
-        pulses3++;        
-    }
-
-    lastflowpinstate = x;
-    lastflowpinstate3 = y;
-    flowrate = 1000.0;
-    flowrate3 = 1000.0;
-    flowrate /= lastflowratetimer;  // Em hertz
-    flowrate3 /= lastflowratetimer3;  // Em hertz
-    lastflowratetimer = 0;
-    lastflowratetimer3 = 0;
+SIGNAL(TIMER0_COMPA_vect) {
+  uint8_t x = digitalRead(FLOWSENSORPIN1);
+  
+  if (x == lastflowpinstate) {
+    lastflowratetimer++;
+    return; // nada mudou!
+  }
+  
+  if (x == HIGH) {
+    //low to high  transição!
+    pulses++;
+  }
+  lastflowpinstate = x;
+  flowrate = 1000.0;
+  flowrate /= lastflowratetimer;  // Em hertz
+  lastflowratetimer = 0;
+}
+SIGNAL(TIMER1_COMPA_vect) {
+  uint8_t y = digitalRead(FLOWSENSORPIN2);
+  
+  if (y == lastflowpinstate2) {
+    lastflowratetimer2++;
+    return; // nada mudou!
+  }
+  
+  if (y == HIGH) {
+    //low to high  transição!
+    pulses2++;
+  }
+  lastflowpinstate2 = y;
+  flowrate2 = 1000.0;
+  flowrate2 /= lastflowratetimer2;  // Em hertz
+  lastflowratetimer2 = 0;
 }
 
-void useInterrupt (boolean v)
-{
-    if (v)
-    {
-        // Timer0 já é usado para millis () - nós vamos apenas interromper em algum lugar
-        // no meio e chamar a função "Compare A" acima
-        OCR0A = 0xAF;
-        TIMSK0 |= _BV (OCIE0A);
-    }
-    else
-    {
-        // Não chamar mais a função de interrupção COMPA
-        TIMSK0 &= ~_BV (OCIE0A);
-    }
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 já é usado para millis () - nós vamos apenas interromper em algum lugar
+    // no meio e chamar a função "Compare A" acima
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+  } else {
+    // Não chamar mais a função de interrupção COMPA
+    TIMSK0 &= ~_BV(OCIE0A);
+  }
+}
+void useInterrupt2(boolean w) {
+  if (w) {
+    // Timer0 já é usado para millis () - nós vamos apenas interromper em algum lugar
+    // no meio e chamar a função "Compare A" acima
+    OCR0A = 0xAF;
+    TIMSK1 |= _BV(OCIE0A);
+  } else {
+    // Não chamar mais a função de interrupção COMPA
+    TIMSK1 &= ~_BV(OCIE0A);
+  }
 }
 
 void setup()
@@ -76,13 +79,14 @@ void setup()
     Serial.begin (9600);
     Serial.print ("Sensor de fluxo OK!");
 
-    pinMode (FLOWSENSORPIN, INPUT);
-    pinMode (FLOWSENSORPIN3, INPUT);
-    digitalWrite (FLOWSENSORPIN, HIGH);
-    digitalWrite (FLOWSENSORPIN3, HIGH);
-    lastflowpinstate = digitalRead (FLOWSENSORPIN);
-    lastflowpinstate3 = digitalRead (FLOWSENSORPIN3);
-    useInterrupt (true);
+   pinMode(FLOWSENSORPIN1, INPUT);
+   digitalWrite(FLOWSENSORPIN1, HIGH);
+   lastflowpinstate = digitalRead(FLOWSENSORPIN1);
+   useInterrupt(true);
+   pinMode(FLOWSENSORPIN2, INPUT);
+   digitalWrite(FLOWSENSORPIN2, HIGH);
+   lastflowpinstate = digitalRead(FLOWSENSORPIN2);
+   useInterrupt2(true);
 }
 
 void loop()                     // executar uma e outra vez
@@ -102,9 +106,9 @@ void loop()                     // executar uma e outra vez
     liters /= 7.5;
     liters /= 60.0;
 
-    float liters3 = pulses3;
-    liters3 /= 7.5;
-    liters3 /= 60.0;
+    float liters2 = pulses2;
+    liters2 /= 7.5;
+    liters2 /= 60.0;
 
     /*
         // Se for um sensor de bronze utilizar o seguinte cálculo
@@ -113,11 +117,28 @@ void loop()                     // executar uma e outra vez
         liters -= 6;
         liters /= 60.0;
     */
-    Serial.print (liters);
-    Serial.print (" Litros - Sensor 1  /  ");
+   // Serial.print (liters);
+   // Serial.print (" Litros - Sensor 1  /  ");
 
-    Serial.print (liters3);
-    Serial.println (" Litros - Sensor 2");
+   // Serial.print (liters2);
+   // Serial.print (" Litros - Sensor 2  /  ");
 
+
+    int percent = (liters - liters2) / liters2 * 100; //Calcula o percentual das leituras
+
+    // Serial.print (percent);                       
+    // Serial.println (" Percentual  /  ");
+
+    // Condições para o funcionamento do ponteiro
+    if (percent < 0){
+    percent = 0;
+    } else if (percent > 100){
+    percent = 100;
+    }
+    percent = percent * 1.8;
+
+    Serial.write (percent); //Envia para a interface (Processing) o valor caculado entre os sensores
+                           //que será um valor de 0 a 180
+   
     delay (100);
 }
